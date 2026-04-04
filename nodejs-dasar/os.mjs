d7b8ea5f8670487db36a68104a1e0ff1b7ccaceb
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-04-04 19:51:49 +0700

    OS

---
+++ nodejs-dasar/os.mjs
@@ -0,0 +1,12 @@
+import os from 'os';
+
+console.log(os.arch());
+console.log(os.platform());
+console.table(os.cpus());
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.homedir());
+console.log(os.hostname());
+console.table(os.networkInterfaces());
+console.log(os.uptime());
+console.log(os.userInfo());
