9fecf0abf898a166d22155b74ecdbc9909c0216d
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-04-04 19:06:59 +0700

    NodeJS Standard Library

---
+++ nodejs-dasar/standard-library.mjs
@@ -0,0 +1,5 @@
+//agar dapat menggunakan module es6 maka tambahkan "type": "module" pada file package.json/nama file diakhiri dengan .mjs
+import os from 'os';
+
+console.log(os.platform());
+console.log(os.cpus());
