9fa816538fce22a28ab02e8eacfff5a88addc4b5
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-04-07 22:45:37 +0700

    Update Dependency

--- nodejs-npm/package.js
+++ nodejs-npm/package.js
@@ -1,4 +1,8 @@
 import { sayHello, sum } from "nodejs-membuat-library";
+import { min, max } from "nodejs-membuat-library/number";
 
 console.log(sayHello('Joko'));
 console.log(sum([1, 2, 3, 4, 5]));
+
+console.log(min(30, 40));
+console.log(max(30, 40));
