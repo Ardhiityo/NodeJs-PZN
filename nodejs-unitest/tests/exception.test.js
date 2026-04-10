3983be4383647fd4ef3710da28b706ce4161f91a
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-04-10 23:59:08 +0700

    Code Coverage

--- nodejs-unitest/tests/exception.test.js
+++ nodejs-unitest/tests/exception.test.js
@@ -1,6 +1,9 @@
 import { callMe, MyException } from "../src/exception.js";
 
 test('test exception', () => {
+    //Memastikan tidak terjadi exception
+    expect(() => callMe('Eko')).not.toThrow();
+
     //Memastikan terjadi exception apapun
     expect(() => callMe()).toThrow();
 
