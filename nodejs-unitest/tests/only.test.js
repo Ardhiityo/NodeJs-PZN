de5dcc852cf2d609e8f67958832fb228c46a3180
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-04-11 15:07:47 +0700

    Only Function

---
+++ nodejs-unitest/tests/only.test.js
@@ -0,0 +1,9 @@
+describe('My Feature', () => {
+    it('should do something', () => {
+        expect(true).toBe(true);
+    });
+
+    it.only('should do something else', () => {
+        expect(true).toBe(true);
+    });
+});
