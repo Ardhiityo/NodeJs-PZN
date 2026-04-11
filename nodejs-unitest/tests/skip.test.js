09ab5d8df9601b3cc6ffb12d6c49699e41450427
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-04-11 12:48:14 +0700

    Skip Function

---
+++ nodejs-unitest/tests/skip.test.js
@@ -0,0 +1,9 @@
+describe('My Feature', () => {
+    it('should do something', () => {
+        expect(true).toBe(true);
+    });
+
+    it.skip('should do something else', () => {
+        expect(true).toBe(true);
+    });
+});
