df6b592a2306e3338c0549511beb2f998999d151
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-04-15 20:22:29 +0700

    Basic Routing

--- nodejs-express/src/hello.js
+++ nodejs-express/src/hello.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
 app.listen(3000, function () {
     console.log('App listen on port : 3000');
 });
