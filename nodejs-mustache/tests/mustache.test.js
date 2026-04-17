5c6e6730fb3ddf8a26a26f3917410256783adf84
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-04-17 20:12:44 +0700

    Mustache Cache

--- nodejs-mustache/tests/mustache.test.js
+++ nodejs-mustache/tests/mustache.test.js
@@ -1,6 +1,16 @@
 import Mustache from "mustache";
 
 test("mustache render", () => {
+    //proses render template, proses cache dilakukan ketika memanggil render
+    const template = Mustache.render("Hello {{name}}", { name: "John" });
+    expect(template).toBe("Hello John");
+});
+
+test("mustache cache", () => {
+    //proses compile template
+    Mustache.parse("Hello {{name}}");
+
+    //proses render template, langsung menggunakan template yang sudah di compile
     const template = Mustache.render("Hello {{name}}", { name: "John" });
     expect(template).toBe("Hello John");
 });
