bd761713b3837bf868c43ab15fdbc0659f079584
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-04-17 20:19:30 +0700

    Tags

--- nodejs-mustache/tests/mustache.test.js
+++ nodejs-mustache/tests/mustache.test.js
@@ -14,3 +14,15 @@ test("mustache cache", () => {
     const template = Mustache.render("Hello {{name}}", { name: "John" });
     expect(template).toBe("Hello John");
 });
+
+test("mustache tags", () => {
+    //proses render template, langsung menggunakan template yang sudah di compile
+    const template = Mustache.render("Hello {{name}} my hobby is {{{hobby}}}",
+        {
+            name: "John",
+            hobby: "<b>Coding</b>"
+        }
+    );
+
+    expect(template).toBe("Hello John my hobby is <b>Coding</b>");
+});
