07d9218fa9fa6ff45bf8f0d135348b0baab550cf
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-04-25 15:26:21 +0700

    Date Validation

---
+++ nodejs-validation/tests/date.test.js
@@ -0,0 +1,20 @@
+import Joi from "joi";
+
+test("validation date with iso", () => {
+    const schema = Joi.object({
+        birthDate: Joi.date()
+            .required()
+            .max("now")
+            .min("1988-01-01")
+            .iso()
+    });
+
+    const data = {
+        birthDate: "1999-05-05"
+    };
+
+    const { error, value } = schema.validate(data);
+
+    expect(error).toBeUndefined();
+    expect(value.birthDate).toEqual(new Date("1999-05-05"));
+});
