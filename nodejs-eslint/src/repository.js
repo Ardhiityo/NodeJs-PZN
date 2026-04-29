314006440babc7d57b20127046636e92eb7a5d44
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-04-29 14:48:21 +0700

    Formatter

--- nodejs-eslint/src/repository.js
+++ nodejs-eslint/src/repository.js
@@ -6,3 +6,6 @@ export const sum = (a, b) => {
     let total = a + b; //error : total is never reassigned
     return total;
 };
+
+//jalankan di cmd : npx eslint src --format html > eslint.html
+//akan membuat file eslint.html berisi error nya
