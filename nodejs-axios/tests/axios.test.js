4fee36e99bdb418a0272fe3723bd64645dbf1bc9
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-04-30 20:52:14 +0700

    HTTP Method

--- nodejs-axios/tests/axios.test.js
+++ nodejs-axios/tests/axios.test.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+const instance = axios.create({
+    baseURL: "https://eovkp0h5dtd7x0w.m.pipedream.net",
+    timeout: 5000,
+    headers: { "X-Custom-Header": "foobar" },
+});
+
 test('Should support http client', () => {
     const instance = axios.create({
         baseURL: "https://eovkp0h5dtd7x0w.m.pipedream.net",
@@ -8,4 +14,12 @@ test('Should support http client', () => {
     });
 
     expect(instance).toBeDefined();
+});
+
+test('Should support http method', async () => {
+
+    const response = await instance.get();
+    
+    expect(response).toBeDefined();
+    expect(response.status).toBe(200);
 });
